85bd7f2926507d7832be37d145c181b7b847ed1c
Muhammad Rabi <vonrabi07@yahoo.com>
2021-11-03 11:13:55 +0200

    tweaking

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,54 @@ navToggle.addEventListener("click", () => {
   navToggle.classList.toggle("active");
   navBar.classList.toggle("active");
 });
+
+// navigation
+
+let navListItems = document.querySelectorAll("header ul a");
+
+function scrollToSection(navLinks) {
+  navLinks.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      e.preventDefault();
+      // remove any active class on navLink on click
+      navLinks.forEach((item) => item.classList.remove("active"));
+      // add active class when clicking a nav link
+      e.currentTarget.classList.add("active");
+      // smooth scroll to desired section automatically
+      let sectionToGo = document.querySelector(e.target.dataset.nav).offsetTop;
+      window.scrollTo({
+        top: sectionToGo,
+        behaviour: "smooth",
+      });
+    });
+  });
+}
+
+scrollToSection(navListItems);
+
+// filter items logic
+let galleryLists = document.querySelectorAll(".gallery-list li a");
+let galleryItems = document.querySelectorAll(".gallery figure");
+
+function filterItems(triggers) {
+  triggers.forEach((trigger) => {
+    trigger.addEventListener("click", (e) => {
+      e.preventDefault();
+      // remove active class from active triggers
+      triggers.forEach((list) => {
+        list.classList.remove("active");
+      });
+      e.currentTarget.classList.add("active");
+
+      // set the opacity of all images to 0.5
+      galleryItems.forEach((item) => (item.style.opacity = "0.5"));
+      // change the opacity for clicked items to 1
+      document
+        .querySelectorAll(e.target.dataset.make)
+        .forEach((item) => (item.style.opacity = "1"));
+    });
+  });
+}
+
+//calling function
+filterItems(galleryLists);
